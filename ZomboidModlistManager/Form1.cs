d326ab4ce51676af310f094be19ca5fa0d7a4cc0
Phoney <fajthlaci@gmail.com>
2023-04-24 17:51:17 +0200

    commit

--- ZomboidModlistManager/Form1.cs
+++ ZomboidModlistManager/Form1.cs
@@ -197,6 +197,7 @@ namespace ZomboidModlistManager
                                     }
                                 }
                             }
+                            break;
                         }
 
                         modName = modName.Trim();
